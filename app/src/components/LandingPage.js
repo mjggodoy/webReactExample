c5114bf62f0bacd6c8725e38ab1482aaacaff77b
marichu <mjggodoy@gmail.com>
2018-10-31 09:46:28 +0100

    Some "stupid" corrections in the the text

--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -52,16 +52,15 @@ const Body = () => {
       </div>
       <div className="container text-center">
         <p className="lead">
-        There are many vocabularies and OWL representations which are being produced about agronomic data for the last few years.
-        However, there are so many data sources that are spread out that stored in different formats and standards. 
-        Therefore, the objective of this project is to create a common platform that stores information in which different standards 
-        follow the principles of Linked Open Data. These principles support the use of standards (RDF) and vocabularies (SKOS). 
-        The data model that was used correspond to an OWL representation very accepted by the scientific community in the agronomic domain.
+        There are many vocabularies and OWL representations that are being produced about agronomic data for the last few years.
+        However, there are so many data sources that are spread out and have been stored in different formats and standards. 
+        Therefore, the objective of this project is to create a common platform that stores information in different standards that follow
+        the principles of Open Linked Data. These principles support the use of standards (RDFs and OWLs) and vocabularies (SKOS). In this context, 
+        one of the main objectives is to provide a data scheme to integrate the converted RDF agronomioc information retreived from diffetent data sources. 
         </p>
       </div>
     </header> 
   );
 }
-
 
 export default LandingPage;
